8594d86ec1f8d0547a90d9f8194cf6d72ba0f0ee
Mirza Mesinovic <mirza.mesinovic@chasacademy.com>
2024-05-02 01:04:43 +0200

    Basiv styling for homepage

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import css from "./Welcome.module.css";
 
 const Welcome = ({ toggleCollapse, asideIsCollapsed }) => {
   return (
@@ -6,7 +7,7 @@ const Welcome = ({ toggleCollapse, asideIsCollapsed }) => {
       <button className="collapse_button" onClick={toggleCollapse}>
         {asideIsCollapsed ? ">" : "<"}
       </button>
-      <div>
+      <div className={css.welcome_container}>
         <h1>Welcome to Kanban Kingdom!!</h1>
         <p>This is the landing page of your kanban application.</p>
         <p> ⬅️ Select or create a new Board to start your project!</p>

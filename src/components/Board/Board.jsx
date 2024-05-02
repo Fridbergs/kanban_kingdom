671f7118115936f4bd2a54fe483be871da63e1ae
Mirza Mesinovic <mirza.mesinovic@chasacademy.com>
2024-05-02 19:10:46 +0200

    Implemented functionality to change board names

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Column from "../Column/Column";
-import { addColumn } from "../../slices/BoardSlice";
+import { addColumn, editBoardName } from "../../slices/BoardSlice";
 import css from "./Board.module.css";
 import { useParams } from "react-router-dom";
 
@@ -13,26 +13,48 @@ const Board = ({ handleOpenModal, toggleCollapse, asideIsCollapsed }) => {
   const board = boards.find((board) => board.id === boardId);
 
   const [input, setInput] = useState("");
+  // const [newBoardName, setNewBoardName] = useState(board.title);
+  const [isEditingBoardName, setIsEditingBoardName] = useState(false);
   const dispatch = useDispatch();
 
   const handleAddColumn = (e) => {
     e.preventDefault();
-    dispatch(addColumn({ title: input, boardId }));
+    dispatch(addColumn({ title: input, boardId: board.id }));
     setInput("");
   };
 
+  function handleBoardNameChange(e) {
+    dispatch(editBoardName({ boardName: e.target.value, boardId: board.id }));
+  }
+
   if (!board) {
     // Handle case when board is not found
-    return <div>Loading...</div>;
+    return <div style={{ padding: "10px 20px 20px 20px" }}>Loading...</div>;
   }
 
   return (
     <main className={css.board}>
       <div className={css.board_header}>
-        <button className="collapse_button no_margin" onClick={toggleCollapse}>
-          {asideIsCollapsed ? ">" : "<"}
-        </button>
-        <h2>{board.title}</h2>
+        <div className={css.left_side}>
+          <button
+            className="collapse_button no_margin"
+            onClick={toggleCollapse}>
+            {asideIsCollapsed ? ">" : "<"}
+          </button>
+          {isEditingBoardName ? (
+            <input
+              type="text"
+              className={css.boardname_edit_input}
+              onChange={handleBoardNameChange}
+              onKeyDown={(e) =>
+                e.key === "Enter" && setIsEditingBoardName(false)
+              }
+              value={board.title}
+            />
+          ) : (
+            <h2 onClick={() => setIsEditingBoardName(true)}>{board.title}</h2>
+          )}
+        </div>
         <form onSubmit={handleAddColumn}>
           <input
             type="text"

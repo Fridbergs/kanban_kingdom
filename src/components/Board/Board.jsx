02d305d7645373957795a9ca4c6e3e7d076277e9
Mirza Mesinovic <mirza.mesinovic@chasacademy.com>
2024-05-02 20:18:03 +0200

    added autofocus on boardname edit

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Column from "../Column/Column";
 import { addColumn, editBoardName } from "../../slices/BoardSlice";
@@ -7,13 +7,12 @@ import { useParams } from "react-router-dom";
 
 const Board = ({ handleOpenModal, toggleCollapse, asideIsCollapsed }) => {
   const { boardId } = useParams();
+  const focusBoardNameEdit = useRef(null);
   const boards = useSelector((state) => state.boards);
 
   // Find the board using the parsed boardId as a string
   const board = boards.find((board) => board.id === boardId);
-
   const [input, setInput] = useState("");
-  // const [newBoardName, setNewBoardName] = useState(board.title);
   const [isEditingBoardName, setIsEditingBoardName] = useState(false);
   const dispatch = useDispatch();
 
@@ -26,6 +25,23 @@ const Board = ({ handleOpenModal, toggleCollapse, asideIsCollapsed }) => {
   function handleBoardNameChange(e) {
     dispatch(editBoardName({ boardName: e.target.value, boardId: board.id }));
   }
+
+  function handleToggleBoardNameEdit() {
+    setIsEditingBoardName((prev) => !prev);
+    if (focusBoardNameEdit.current) {
+      focusBoardNameEdit.current.focus();
+    }
+  }
+
+  function handleKeyPress(e) {
+    if (e.key === "Enter") handleToggleBoardNameEdit();
+  }
+
+  useEffect(() => {
+    if (focusBoardNameEdit.current) {
+      focusBoardNameEdit.current.focus();
+    }
+  }, [isEditingBoardName]);
 
   if (!board) {
     // Handle case when board is not found
@@ -46,13 +62,12 @@ const Board = ({ handleOpenModal, toggleCollapse, asideIsCollapsed }) => {
               type="text"
               className={css.boardname_edit_input}
               onChange={handleBoardNameChange}
-              onKeyDown={(e) =>
-                e.key === "Enter" && setIsEditingBoardName(false)
-              }
+              onKeyDown={handleKeyPress}
               value={board.title}
+              ref={focusBoardNameEdit}
             />
           ) : (
-            <h2 onClick={() => setIsEditingBoardName(true)}>{board.title}</h2>
+            <h2 onClick={handleToggleBoardNameEdit}>{board.title}</h2>
           )}
         </div>
         <form onSubmit={handleAddColumn}>
